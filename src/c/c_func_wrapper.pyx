fe7ba599f992ee661ac6da4bc2eb96b2cefab614
shtmjp <shtm.1058@gmail.com>
2025-01-02 01:57:51 +0900

    remove unnecessary dependencies

--- src/c/c_func_wrapper.pyx
+++ src/c/c_func_wrapper.pyx
@@ -1,13 +1,11 @@
 import numpy as np
 cimport numpy as cnp
+cimport c_func_wrapper
 
-cdef extern from "c_add.h":
-    double sum_array_c(double *array, int n)
-    
 def sum_array(cnp.ndarray[double, ndim=1, mode="c"] array):
     cdef int n = array.shape[0]
     cdef double *c_array
     c_array = <double*> array.data
     cdef double res
-    res = sum_array_c(c_array, n)
+    res = c_func_wrapper.sum_array_c(c_array, n)
     return res
